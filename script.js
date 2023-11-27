f26675e3011ba69a452f410d3e89a88e86d25064
fight <fightme4ls@gmail.com>
2023-11-26 17:27:51 -0800

    x

---
+++ script.js
@@ -0,0 +1,21 @@
+var goal = "";
+var length = "";
+var reps = "";
+var lifts = "";
+var sets = "";
+var other = "";
+function getFormValues(){
+  goal = document.getElementById('goal').value;
+  length = document.getElementById('length').value;
+  reps = document.getElementById('reps').value;
+  lifts = document.getElementById('lifts').value;
+  sets = document.getElementById('sets').value;
+  other = document.getElementById('other').value;
+
+  updateGoal();
+}
+
+function updateGoal(){
+
+  
+}

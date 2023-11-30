28ec5a535e246a91250c9f59fa3b648adb7f1202
fight <fightme4ls@gmail.com>
2023-11-30 10:06:15 -0800

    dropdown

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 var goal = "";
+var output = "";
 var length = "";
 var reps = "";
 var lifts = "";
 var sets = "";
 var other = "";
+var text = "";
 
 function getFormValues(){
-  goal = document.getElementById('goal').value;
+  goal = document.querySelector('#dropdown').value;
   length = document.getElementById('length').value;
   reps = document.getElementById('reps').value;
   lifts = document.getElementById('lifts').value;
   sets = document.getElementById('sets').value;
   other = document.getElementById('other').value;
+  console.log(goal);
   updateGoal();
 }
 

c36b4386f7e3a676e1cd75abea1fb70704db12d3
fightme4ls <fightme4ls@gmail.com>
2023-12-02 22:55:01 -0800

    user input acquired o7

--- public/script.js
+++ public/script.js
@@ -44,6 +44,13 @@ function swapFormToRun(){
   document.getElementById("workoutPageButton").style.display = "none";
 }
 
+function resetForm(){
+    document.getElementById("runInputs").style.display = "none";
+    document.getElementById("weightInputs").style.display = "none";
+    document.getElementById("placeholder").style.display = "block";
+    document.getElementById("workoutPageButton").style.display = "none";
+}
+
 function switchToCreateAccountPage() {
     window.location.href = "create.html";
 }
@@ -64,3 +71,47 @@ function getFormValues() {
 function switchToWorkoutPage() {
     window.location.href = "workout.html";
 }
+
+function addWorkout() {
+    var table = document.getElementById("workoutTable");
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var input = document.createElement('input');
+    var input1 = document.createElement('input');
+    var input2 = document.createElement('input');
+    var input3 = document.createElement('input');
+    input.type = 'text';
+    input1.type = 'text';
+    input2.type = 'text';
+    input3.type = 'text';
+    cell1.appendChild(input);
+    cell2.appendChild(input1);
+    cell3.appendChild(input2);
+    cell4.appendChild(input3);
+  }
+
+  function removeWorkout() {
+    var table = document.getElementById("workoutTable");
+    if (table.rows.length > 1) {
+      table.deleteRow(1);
+    }
+  }
+
+  var date = "";
+  function getInputs() {
+    date = document.getElementById("date").value;
+    console.log(date);
+    var table = document.getElementById("workoutTable");
+    var rows = table.getElementsByTagName("tr");
+    for (var i = 1; i < rows.length; i++) {
+      var cells = rows[i].getElementsByTagName("td");
+      var exercise = cells[0].getElementsByTagName("input")[0].value;
+      var sets = cells[1].getElementsByTagName("input")[0].value;
+      var reps = cells[2].getElementsByTagName("input")[0].value;
+      var weight = cells[3].getElementsByTagName("input")[0].value;
+      console.log("Exercise: " + exercise + ", Sets: " + sets + ", Reps: " + reps + ", Weight: " + weight);
+    }
+  }

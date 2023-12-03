a1e4cc52ebe10426ef341c5bb3413ab69e53911e
JoSpaghetti <143671979+JoSpaghetti@users.noreply.github.com>
2023-12-02 21:09:50 -0800

    small changes
    multiplied bar width by 100

--- youtube+bar.js
+++ youtube+bar.js
@@ -6,7 +6,7 @@ function moveBar() {
     var currentWeight = getCurrentWeight();
     var weightDiff = Math.abs(targetWeight - currentWeight);
     console.log (weightDiff);
-    barWidth = (weightDiff/targetWeight + "%");
+    barWidth = (100 * (weightDiff/targetWeight) + "%");
 }
 
 const vidoes = ["testURL", "testURL2"];
@@ -19,17 +19,3 @@ function setVideos() {
         document.body.appendChild(videoIframe);
     }
 }
-
-<div class="videos" id="videos">
-          
-          <script>
-            const videos = ["https://www.youtube.com/embed/cuh5HsU7WQQ?si=5-VJZ4icUg3ZMJTt", "https://www.youtube.com/embed/aui0gg_Zstc?si=YChOOZaFjqHcnCA2,"]
-            for (let i = 0; i < 2; i++) {
-                var iframe = document.createElement("iframe");
-                var randomInt = Math.floor(Math.random() * videos.length);
-                var iframeHTML = "<iframe id='frame" + i + "' className='move' title='YouTube video player' src=' "+ videos[randomInt] +" 'frameborder='0' width = 256px allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowFullScreen></iframe>";
-                document.getElementById("videos").innerHTML += iframeHTML;
-            }
-          </script>
-
-        </div>
